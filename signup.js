a140ffca1bc4ba362511a55ed2ba5794dfb55a20
hilbertdev <hilbertdev@gmail.com>
2019-08-19 17:38:08 +0200

    initial setup

---
+++ signup.js
@@ -0,0 +1,10 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const userName = document.getElementById('userName');
+const passWord = document.getElementById('passWord');
+const conFirm = document.getElementById('conFirm');
+const btnSignup = document.getElementById('btnSignup');
+
+btnSignup.addEventListener('submit', ()=>{
+    
+});
